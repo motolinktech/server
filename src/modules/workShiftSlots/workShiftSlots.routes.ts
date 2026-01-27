3844e87630e5fd96c99131430f9247448437150a
John Vilela <johnvvilela@gmail.com>
2026-01-27 17:42:33 -0300

    feat: add checkInAt and checkOutAt fields to WorkShiftSlotResponse

--- src/modules/workShiftSlots/workShiftSlots.routes.ts
+++ src/modules/workShiftSlots/workShiftSlots.routes.ts
@@ -26,6 +26,8 @@ const WorkShiftSlotResponse = t.Composite([
   t.Object({
     deliverymanAmountDay: t.String(),
     deliverymanAmountNight: t.String(),
+    checkInAt: t.Nullable(t.Date()),
+    checkOutAt: t.Nullable(t.Date()),
   }),
 ]);
 
